1fc9c0668822720167e241c115c59babe1aff4fa
Mark <mshuaic@hotmail.com>
2018-06-07 17:11:00 -0400

    push the wrong file in the last commit

--- test.py
+++ test.py
@@ -1,10 +1,47 @@
-1522000002801	1	1	1	1	REQ_RESOURCE	MOD_UCSC_Genome_Bioinformatics
-1522000008352	1	2	1	1	VIEW_RESOURCE	MOD_UCSC_Genome_Bioinformatics
-1522000016966	1	3	3	6	REQ_RESOURCE	MOD_FlyBase
-1522000019451	1	4	1	1	FILE_ACCESS	MOD_UCSC_Genome_Bioinformatics
-1522000019566	1	5	3	6	VIEW_RESOURCE	MOD_FlyBase
-1522000024848	1	6	6	10	REQ_RESOURCE	GTEx
-1522000030689	1	7	1	1	FILE_ACCESS	MOD_UCSC_Genome_Bioinformatics
-1522000034174	1	8	8	4	REQ_RESOURCE	MOD_Gene_Ontology_Consortium
-1522000037149	1	9	9	10	REQ_RESOURCE	MOD_SGD
-1522000042742	1	10	6	10	VIEW_RESOURCE	GTEx
+from Savoir import Savoir
+import re
+
+# node0 rpc credential
+node0 = {
+    "rpcuser": 'multichainrpc',
+    "rpcpasswd": 'F7ZnmKHnA3GWX68DoGSyAWfG9xmp6Kj3d3btjLZNLmo4',
+    "rpcport": '4394',
+    "rpchost": '34.202.165.15',
+    "chainname": 'chain1'
+}
+
+# node1 rpc credential
+node1 = {
+    "rpcuser": 'multichainrpc',
+    "rpcpasswd": '5wJGtHcdCLXsVYXuMBXicmxV6VqhWxFE1PwMLuGEcoPk',
+    "rpcport": '8001',
+    "rpchost": '34.202.165.15',
+    "chainname": 'chain1'
+}
+
+# connect to nodes
+api0 = Savoir(node0["rpcuser"], node0["rpcpasswd"],
+              node0["rpchost"], node0["rpcport"], node0["chainname"])
+api1 = Savoir(node1["rpcuser"], node1["rpcpasswd"],
+              node1["rpchost"], node1["rpcport"], node1["chainname"])
+# print(api0.getinfo())
+# print(api1.getinfo())
+
+
+# api0.create('stream', 'stream1', True)
+# api0.liststreams('stream1')
+# api1.liststreams('stream1')
+
+
+api0.subscribe('stream1')
+api1.subscribe('stream1')
+
+# read log file
+lines = [re.sub('\s+', ' ', line) for line in open('test1.txt')]
+# print(lines)
+# for line in lines:
+# print(line)
+# api0.publish('stream1', 'node0', line.encode('utf-8').hex())
+
+for item in api1.liststreamkeyitems('stream1', 'node0'):
+    print(bytes.fromhex(item['data']).decode('utf-8'))
